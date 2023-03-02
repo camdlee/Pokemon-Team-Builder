ca2c7c202b90d62e90c480184b32ed5db3566439
Cameron Lee <camdlee18@gmail.com>
2023-03-01 19:17:28 -0500

    fourth commit

--- app/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-
-
-class PokemonForm(FlaskForm):
-    pokemon_name = StringField('name', validators=[DataRequired()])
-    submit_btn = SubmitField('Submit')
---
+++ app_folders/forms.py
@@ -0,0 +1,8 @@
+# from flask_wtf import FlaskForm
+# from wtforms import StringField, SubmitField
+# from wtforms.validators import DataRequired
+
+
+# class PokemonForm(FlaskForm):
+#     pokemon_name = StringField('name', validators=[DataRequired()])
+#     submit_btn = SubmitField('Submit')
